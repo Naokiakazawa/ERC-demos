93594ba0175985ac8917571a4033607859dc74a6
Naokiakazawa <naoki9akazawa@gmail.com>
2020-11-25 05:45:42 +0900

    build sample contract

--- hardhat/src/hardhat.config.js
+++ hardhat/src/hardhat.config.js
@@ -37,6 +37,12 @@ module.exports = {
   },
   // This is a sample solc configuration that specifies which version of solc to use
   solidity: {
-    version: "0.7.3",
+    version: "0.7.5",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
   },
 };
